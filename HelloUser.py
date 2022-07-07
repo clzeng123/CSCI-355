9f203a46b2a906ff554c6493ba7dbc11afe9c42a
charlie zeng <clzeng123@gmail.com>
2022-07-06 22:51:12 -0400

    mew

---
+++ HelloUser.py
@@ -0,0 +1,8 @@
+# get input from user
+first_name = input("Please enter your first name: ")
+last_name =  input("Please enter your last name: ")
+# write output to console
+print("Hello", first_name, last_name)
+
+
+
