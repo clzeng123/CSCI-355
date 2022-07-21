5a1357e5e3dc338e8305376b895aece0c900aef3
Charlie Zeng <clzeng123@gmail.com>
2022-07-21 17:00:09 -0400

    project

--- Assignment 4/UpdateBrowserPage.js
+++ Assignment 4/UpdateBrowserPage.js
@@ -1,25 +1,47 @@
-function UpdateBrowserPage(){
+function UpdateBrowserPage() {
+  document.getElementById("innerHeight").innerHTML =
+    "innerHeight=" + window.innerHeight;
+  document.getElementById("innerWidth").innerHTML =
+    "innerWidth=" + window.innerWidth;
+  document.getElementById("AppName").innerHTML = "appName=" + navigator.appName;
+  document.getElementById("product").innerHTML = "product=" + navigator.product;
+  document.getElementById("appVersion").innerHTML =
+    "appVersion=" + navigator.appVersion;
+  document.getElementById("userAgent").innerHTML =
+    "userAgent=" + navigator.userAgent;
+  document.getElementById("platform").innerHTML =
+    "platform=" + navigator.platform;
+  document.getElementById("language").innerHTML =
+    "language=" + navigator.language;
+  document.getElementById("screen_width").innerHTML = "width=" + screen.width;
+  document.getElementById("screen_height").innerHTML =
+    "height=" + screen.height;
+  document.getElementById("screen_availWidth").innerHTML =
+    "availWidth=" + screen.availWidth;
+  document.getElementById("screen_availHeight").innerHTML =
+    "availHeight=" + screen.availHeight;
+  document.getElementById("screen_colorDepth").innerHTML =
+    "colorDepth=" + screen.colorDepth;
+  document.getElementById("screen_pixelDepth").innerHTML =
+    "pixelDepth=" + screen.pixelDepth;
+  document.getElementById("href").innerHTML = "href=" + location.href;
+  document.getElementById("hostname").innerHTML =
+    "hostname=" + location.hostname;
+  document.getElementById("pathname").innerHTML =
+    "pathname=" + location.pathname;
+  document.getElementById("protocol").innerHTML =
+    "protocol=" + location.protocol;
 
-    document.getElementById("nav_AppName").innerHTML ="App Name: " + Navigator.AppName;
-    document.getElementById("nav_product").innerHTML ="Product: " + Navigator.product;
-    document.getElementById("nav_appVersion").innerHTML ="App Version: " + Navigator.appVersion;
-    document.getElementById("nav_Useragreenment").innerHTML ="User Agreement: " + Navigator.userAgent;
-
-    document.getElementById("win_innerHeight").innerHTML ="inner Height: " + Window.innerHeight+"px";
-    document.getElementById("win_innerWidth").innerHTML ="inner Width: " + Window.innerWidth+"px";
-
-    document.getElementById("screen_width").innerHTML ="Width: " + screen.width+"px";
-    document.getElementById("screen_height").innerHTML ="Height: " + screen.height+"px";
-    document.getElementById("screen_availWidth").innerHTML ="availWidth: " + screen.availWidth+"px";
-    document.getElementById("screen_availHeight").innerHTML ="availHeight: " + screen.availHeight+"px";
-    document.getElementById("screen_colorDepth").innerHTML ="colorDepth: " + screen.colorDepth;
-    document.getElementById("screen_pixelDepth").innerHTML ="pixelDepth: " + screen.pixelDepth;
-
-    document.getElementById("location_hostname").innerHTML ="Hostname: " + location.hostname;
-    document.getElementById("location_pathname").innerHTML ="PathName: " + location.pathname
-    document.getElementById("location_protocol").innerHTML ="Protocol: " + location.protocol;
-
-    document.getElementById("geo_latitude").innerHTML ="Latitude: " + geolocation.latitude;
-    document.getElementById("geo_longitude").innerHTML ="Longitude: " + Geolocation.Longitude;
+  if (navigator.geolocation) {
+    navigator.geolocation.getCurrentPosition(showPosition);
+  } else {
+    x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
 
+function showPosition(position) {
+  document.getElementById("Latitude").innerHTML =
+    "Latitude=" + position.coords.latitude;
+  document.getElementById("Longitude").innerHTML =
+    "Latitude=" + position.coords.longitude;
+}
